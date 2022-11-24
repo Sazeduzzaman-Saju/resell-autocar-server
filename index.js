cdc7a7135d1f7b940969a1521d1d19b9a68deffa
Sazeduzzaman Saju <sazeduzzaman@yahoo.com>
2022-11-24 07:19:53 +0600

    MongoDB Database Connected

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.kystxht.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+
+
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(error => console.error(error))
+
+
+
+
+
+
+
+
+
+
+
 
 
 
